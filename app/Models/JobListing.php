aadc72cfced240d61927fb3069f9a8ab545a0e3e
Saleh <xsaleh2090@gmail.com>
2023-11-03 16:56:27 +0300

    adding the required fields to the JobListing model and migration

--- app/Models/JobListing.php
+++ app/Models/JobListing.php
@@ -8,4 +8,42 @@ use Illuminate\Database\Eloquent\Model;
 class JobListing extends Model
 {
     use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'employer_id',
+        'title',
+        'description',
+        'location',
+        'salary_from',
+        'salary_to',
+        'employemnt_type',
+        'status',
+        'posted_at',
+    ];
+
+
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'employer_id' => 'integer',
+        'title' => 'string',
+        'description' => 'string',
+        'location' => 'string',
+        'salary_from' => 'float',
+        'salary_to' => 'float',
+        'employemnt_type' => 'integer',
+        'status' => 'boolean',
+        'posted_at' => 'datetime',
+    ];
+
+
+    
 }
