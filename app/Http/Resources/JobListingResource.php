54b39e6cd3b4b68f41f1f9e774cad56467add8a2
Saleh <xsaleh2090@gmail.com>
2023-11-04 11:55:27 +0300

    adding the api response trait

---
+++ app/Http/Resources/JobListingResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class JobListingResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'employer_id' => $this->employer,
+            'title' => $this->title,
+            'description' => $this->description,
+            'location' => $this->location,
+            'salary_from' => $this->salary_from,
+            'salary_to' => $this->salary_to,
+            'employemnt_type' => $this->employemnt_type,
+            'status' => $this->status,
+            'posted_at' => $this->posted_at,
+            'expires_at' => $this->expires_at,
+        ];
+    }
+}
